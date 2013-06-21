9eaa4986cff146cad26ce7eb68a554b57794cd1f
FaizalPribadi <faizal_pribadi@aol.com>
2013-06-21 15:45:17 +0700

    @throws errors if not exclude or include the cache providers

---
+++ Exception/DriverException.php
@@ -0,0 +1,23 @@
+<?php
+namespace Mozart\Library\Cache\Exception;
+
+/*
+ * This file is a part of Mozart PHP Small MVC Framework
+ *
+ * (c) Faizal Pribadi <faizal_pribadi@aol.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Class DriverException
+ *
+ * @package Mozart\Library\Cache\Exception
+ * @author  Faizal Pribadi  <faizal_pribadi@aol.com>
+ */
+class DriverException extends \InvalidArgumentException
+{
+
+}
