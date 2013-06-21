08f3788d8a53572e009179c4ed334a878c9e37f1
FaizalPribadi <faizal_pribadi@aol.com>
2013-06-21 15:42:13 +0700

    extending @Mozart\Library\Event\Event

---
+++ Event/CacheEvent.php
@@ -0,0 +1,25 @@
+<?php
+namespace Mozart\Library\Cache\Event;
+
+/*
+ * This file is a part of Mozart PHP Small MVC Framework
+ *
+ * (c) Faizal Pribadi <faizal_pribadi@aol.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * file that was distributed with this source code.
+ */
+
+use Mozart\Library\Event\Event;
+
+/**
+ * Class CacheEvent
+ *
+ * @package Mozart\Library\Event
+ * @author  Faizal Pribadi  <faizal_pribadi@aol.com>
+ */
+class CacheEvent extends Event
+{
+
+}
